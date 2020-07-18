6b8fc1a43a41ebfe740e1efe6ec2643b1058e650
LEANDRO DA SILVA <32754449+leandro47@users.noreply.github.com>
2020-07-18 12:29:32 -0300

    finalizando teste

--- teste.js
+++ teste.js
@@ -40,55 +40,62 @@
 // });
 // console.log(vlrNegativo);
 
-let numero01 = 11257;
+// CALCULANDO O NUMERO DE NOTAS =============================================
 
-function ajuste(nr, casas) {
-    const og = Math.pow(10, casas)
-    return Math.floor(nr * og) / og;
-}
+// let numero01 = 11257;
 
-console.log(numero01);
+// function ajuste(nr, casas) {
+//     const og = Math.pow(10, casas)
+//     return Math.floor(nr * og) / og;
+// }
 
-if ((numero01 / 100) >= 1) {
-    console.log(ajuste((numero01 / 100), 0) + ' nota(s) de R$ 100,00');
-    numero01 = numero01 % 100;
-} else
-    console.log(0 + ' nota(s) de R$ 100,00');
+// console.log(numero01);
 
-if ((numero01 / 50) >= 1) {
-    console.log(ajuste((numero01 / 50), 0) + ' nota(s) de R$ 50,00');
-    numero01 = numero01 % 50;
-} else
-    console.log(0 + ' nota(s) de R$ 50,00');
+// if ((numero01 / 100) >= 1) {
+//     console.log(ajuste((numero01 / 100), 0) + ' nota(s) de R$ 100,00');
+//     numero01 = numero01 % 100;
+// } else
+//     console.log(0 + ' nota(s) de R$ 100,00');
 
-if ((numero01 / 20) >= 1) {
-    console.log(ajuste((numero01 / 20), 0) + ' nota(s) de R$ 20,00');
-    numero01 = numero01 % 20;
-} else
-    console.log(0 + ' nota(s) de R$ 20,00');
+// if ((numero01 / 50) >= 1) {
+//     console.log(ajuste((numero01 / 50), 0) + ' nota(s) de R$ 50,00');
+//     numero01 = numero01 % 50;
+// } else
+//     console.log(0 + ' nota(s) de R$ 50,00');
 
-if ((numero01 / 10) >= 1) {
-    console.log(ajuste((numero01 / 10), 0) + ' nota(s) de R$ 10,00');
-    numero01 = numero01 % 10;
+// if ((numero01 / 20) >= 1) {
+//     console.log(ajuste((numero01 / 20), 0) + ' nota(s) de R$ 20,00');
+//     numero01 = numero01 % 20;
+// } else
+//     console.log(0 + ' nota(s) de R$ 20,00');
 
-} else
-    console.log(0 + ' nota(s) de R$ 10,00');
+// if ((numero01 / 10) >= 1) {
+//     console.log(ajuste((numero01 / 10), 0) + ' nota(s) de R$ 10,00');
+//     numero01 = numero01 % 10;
 
-if ((numero01 / 5) >= 1) {
-    console.log(ajuste((numero01 / 5), 0 )+ ' nota(s) de R$ 5,00');
-    numero01 = numero01 % 5;
+// } else
+//     console.log(0 + ' nota(s) de R$ 10,00');
 
-} else
-    console.log(0 + ' nota(s) de R$ 5,00');
+// if ((numero01 / 5) >= 1) {
+//     console.log(ajuste((numero01 / 5), 0 )+ ' nota(s) de R$ 5,00');
+//     numero01 = numero01 % 5;
 
-if ((numero01 / 2) >= 1) {
-    console.log(ajuste((numero01 / 2),0 )+ ' nota(s) de R$ 2,00');
-    numero01 = numero01 % 2;
+// } else
+//     console.log(0 + ' nota(s) de R$ 5,00');
 
-} else
-    console.log(0 + ' nota(s) de R$ 2,00');
+// if ((numero01 / 2) >= 1) {
+//     console.log(ajuste((numero01 / 2),0 )+ ' nota(s) de R$ 2,00');
+//     numero01 = numero01 % 2;
 
-if ((numero01 / 1) >= 1) {
-    console.log(ajuste((numero01 / 1), 0 )+ ' nota(s) de R$ 1,00');
-} else
-    console.log(0 + ' nota(s) de R$ 1,00');
+// } else
+//     console.log(0 + ' nota(s) de R$ 2,00');
+
+// if ((numero01 / 1) >= 1) {
+//     console.log(ajuste((numero01 / 1), 0 )+ ' nota(s) de R$ 1,00');
+// } else
+//     console.log(0 + ' nota(s) de R$ 1,00');
+
+// let km = 500;
+// let consumo = 35.0;
+
+// console.log((km / consumo).toFixed(3) + ' km/l');
